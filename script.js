9569dcfbaed43733117e378f12af37f43a85ad32
hemantcoder2005 <hemantnarula2203@outlook.com>
2023-12-29 15:42:11 +0530

    basicSetupDone

---
+++ script.js
@@ -0,0 +1,14 @@
+//Storing Theme Prefernce in localStorage
+
+
+
+//ChangeTheme By buuton
+function ChangeTheme() {
+    var body_id=document.getElementById("body")
+    body_id.classList.toggle("bg-black")
+    body_id.classList.toggle("bg-white")
+
+}
+
+
+
